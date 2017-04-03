4192de74b753393919b3c62647b76e7e908cda1b
Krokro <Krokro@Krokro>
2017-04-03 15:28:36 +0200

    @SRV First implementation of the mvc structure

--- src/esi/atl/g39871/poker/mvc/PokerController.java
+++ src/esi/atl/g39871/poker/mvc/PokerController.java
@@ -1,26 +1,67 @@
 package esi.atl.g39871.poker.mvc;
 
 import esi.atl.g39871.poker.model.Game;
+import esi.atl.g39871.poker.model.GameException;
 import esi.atl.g39871.poker.view.PokerView;
+import java.util.Observable;
+import java.util.Observer;
 
-/**
- *
- * @author Krokro
- */
-public class PokerController {
+public class PokerController implements Observer {
+
     Game model;
-    PokerView view;
-    
-    
-    public PokerController(Game model,PokerView view){ //TODO changer la déclaration par les interfaces facade par ex ? 
-        this.model=model;
-        this.view=view;
-        view=new PokerView(); 
 
-        
+
+    PokerView view;
+
+    public PokerController(Game model) { //TODO changer la déclaration par les interfaces facade par ex ? 
+        this.model = model;
+        view = new PokerView(this, model);
+        model.addObserver(this);
+
     }
-    
-    public void addPlayer(String name, int money){
+
+    /**
+     * Add a player to the game, and create a visual for this player
+     *
+     * @param name  the player's name
+     * @param money the player's amount of money
+     */
+    public void addPlayer(String name, int money) {
         model.addPlayer(name, money);
+        view.addLayoutPlayer(model.getPlayers().get(model.getPlayers().size() - 1));
+
+    }
+
+    /**
+     * Start de game and set the start button to invisible
+     *
+     * @throws GameException
+     */
+    public void start() throws GameException {
+        model.start();
+        view.enableStartButton(false);
+    }
+
+    /**
+     * Returns the view's facade
+     *
+     * @return the view's facade
+     */
+    public PokerView getView() {
+        return view;
+    }
+
+    @Override
+    public void update(Observable o, Object o1) {
+
+        if (model.getPlayers().size() >= 4) {
+            view.enableStartButton(true);
+        }
+
+        /*
+         * @SRV update les différents éléments de la vue via la facade de la
+         * vue( PokerView), je mettrai probablement des updates dans chaque vue,
+         * qui seront appelés depuis la classe facade PokerView
+         */
     }
 }
--- src/esi/atl/g39871/poker/view/PokerView.java
+++ src/esi/atl/g39871/poker/view/PokerView.java
@@ -1,18 +1,30 @@
 package esi.atl.g39871.poker.view;
 
 import esi.atl.g39871.poker.model.Game;
+import esi.atl.g39871.poker.model.GameException;
+import esi.atl.g39871.poker.model.Player;
 import esi.atl.g39871.poker.mvc.PokerController;
-import java.awt.TextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
-/**
- * FXML Controller class
+
+/*
+ * @SRV Cette classe sera une "facade" de la vue, c'est à dire qu'elle
+ * comprendra des composants( table, joueur, cartes, etc) mais le controlleur
+ * PokerController interagira avec cette facade, comme il interragit avec la
+ * facade du modèle
  *
- * @author Krokro
+ * Donc tous les autres éléments de l'interface, hormis ceux avec lequel
+ * l'utilisateur va interragir (les boutons) seront des composants à priori. Et
+ * les éléments interactifs seront déclarés ici, dans la facade de la vue.
  */
 public class PokerView extends VBox implements Initializable { //TODO virer initializable ? 
     //TODO est-ce que c'est bien d'étendre VBox ? faut peut-être rien étendre vu que c'est la facade de la vue 
@@ -25,6 +37,14 @@ public class PokerView extends VBox implements Initializable { //TODO virer init
     TextField addPlayerMoney;
 
 
+    @FXML
+    Button startButton;
+
+
+    @FXML
+    Button addPlayerButton;
+
+
     Game model;
 
 
@@ -33,12 +53,42 @@ public class PokerView extends VBox implements Initializable { //TODO virer init
     public PokerView(PokerController controller, Game model) { //TODO passer des interface plutot que des classes concrètes ?
         this.model = model;
         this.controller = controller;
+        FXMLLoader pokerViewFXML = new FXMLLoader(getClass().getResource("PokerView.fxml"));
+
+        pokerViewFXML.setRoot(this);
+        pokerViewFXML.setController(this);
+
+        try {
+            pokerViewFXML.load();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @FXML
-    public void addPlayer() {
+    private void addPlayer() {
         controller.addPlayer(addPlayerName.getText(), Integer.parseInt(addPlayerMoney.getText()));
 
+    }
+
+    @FXML
+    private void start() throws GameException {
+        controller.start();
+    }
+
+    /**
+     * Add a visual component for a new player.
+     *
+     * @param player the new player
+     */
+    public void addLayoutPlayer(Player player) {
+        this.getChildren().add(new Label(player.getName() + " : " + player.getMoney()));
+    }
+
+    public void enableStartButton(boolean b) {
+        startButton.setVisible(b);
     }
 
     /**
